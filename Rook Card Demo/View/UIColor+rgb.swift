f009a136fc7a4e4d9079d04da6c93703341f924b
Steve Liddle <liddle@byu.edu>
2018-10-04 10:51:47 -0600

    Minor refactoring of the UIColor extension.

--- Rook Card Demo/View/UIColor+rgb.swift
+++ Rook Card Demo/View/UIColor+rgb.swift
@@ -8,9 +8,14 @@
 
 import UIKit
 
+extension Int {
+    var rgbComponent: CGFloat {
+        return CGFloat(self) / 255.0
+    }
+}
+
 extension UIColor {
     convenience init(r: Int, g: Int, b: Int) {
-        self.init(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0,
-                  alpha: 1.0)
+        self.init(red: r.rgbComponent, green: g.rgbComponent, blue: b.rgbComponent, alpha: 1.0)
     }
 }
